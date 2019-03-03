3d49f9b32204670b5d8e826db23315ef11b2c2bb
Ali Shalabi <ashalabi13@gmail.com>
2019-03-02 16:17:18 -0800

    Added character, character can move

--- src/GameScene.js
+++ src/GameScene.js
@@ -17,26 +17,74 @@ class GameScene extends Scene {
     const sky = this.add.image(0, 0, 'sky')
     sky.setOrigin(0, 0)
 
-    const star = this.add.image(400, 300, 'star')
-
-  //   const logo = this.add.image(400, 150, 'logo')
-  //   const message = this.add.text(100, 100, 'hello')
-  //   this.input.on('pointerdown', () => {
-  //     message.text = 'ouch'
-  //   })
-  //   this.input.on('pointerup', () => {
-  //     message.text = 'hello'
-  //   })
-  //
-  //   this.tweens.add({
-  //       targets: logo,
-  //       y: 450,
-  //       duration: 2000,
-  //       ease: 'Power2',
-  //       yoyo: true,
-  //       loop: -1
-  //   });
+    this.createPlatform()
+    this.createPlayer()
+    this.createCursor()
   }
+
+  createPlatform() {
+    this.platforms = this.physics.add.staticGroup()
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+    this.platforms.create(600, 400, 'ground')
+    this.platforms.create(50, 250, 'ground')
+    this.platforms.create(750, 220, 'ground')
+  }
+
+  createPlayer() {
+    this.player = this.physics.add.sprite(100, 450, 'dude')
+    this.player.setBounce(0.2)
+
+    this.player.setCollideWorldBounds(true)
+    this.physics.add.collider(this.player, this.platforms)
+
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+  }
+
+  createCursor() {
+    this.cursors = this.input.keyboard.createCursorKeys()
+
+  }
+
+
+  update() {
+        if (this.cursors.left.isDown) {
+          this.player.setVelocityX(-160);
+          this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+          this.player.setVelocityX(160);
+          this.player.anims.play('right', true);
+        }
+        else {
+          this.player.setVelocityX(0);
+          this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+          this.player.setVelocityY(-330);
+        }
+  }
+
+
 }
 
 export default GameScene
--- src/config.js
+++ src/config.js
@@ -6,7 +6,14 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: GameScene
+    scene: GameScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
 }
 
 
